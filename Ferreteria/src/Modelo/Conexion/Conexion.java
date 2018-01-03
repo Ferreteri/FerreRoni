1a86b7fc148742acc0652b8315fc2633834fb48b
DeveloperJavier <javier.vv@hotmail.com>
2018-01-03 17:48:49 -0600

    Estructura de Proyecto

---
+++ Ferreteria/src/Modelo/Conexion/Conexion.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo.Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sistema
+ */
+public class Conexion {
+     static String bd = "DBFerre2018";
+   static String login = "javierVV";
+   static String password = "F4x8.l_%@Rn%";
+   static String url = "jdbc:mysql://23.229.181.103/"+bd;
+
+   Connection conn = null;
+
+   /** Constructor de DbConnection */
+   public Conexion() {
+      try{
+         //obtenemos el driver de para mysql
+         Class.forName("com.mysql.jdbc.Driver");
+         //obtenemos la conexi�n
+         conn = DriverManager.getConnection(url,login,password);
+
+         if (conn!=null){
+            System.out.println("Conecci�n a base de datos "+bd+" OK");
+         }
+      }
+      catch(SQLException e){
+         System.out.println(e);
+      }catch(ClassNotFoundException e){
+         System.out.println(e);
+      }catch(Exception e){
+         System.out.println(e);
+      }
+   }
+   /**Permite retornar la conexi�n*/
+   public Connection getConnection(){
+      return conn;
+   }
+
+   public void desconectar(){
+      conn = null;
+   }
+}
